750b4e2b7cefb5b8a82589ae26b092475b8eaa1a
Abhikhomane45 <abhikhomane45@outlook.com>
2025-08-24 00:46:35 +0530

    flask App created

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/about')
+def about():
+    return "THIS IS ABOUT PAGE"
+
+@app.route('/connect',methods=['GET','POST'])
+def connect():
+    if request.method == 'POST':
+        return "POST request received"
+    else:    
+        return "GET request received"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
